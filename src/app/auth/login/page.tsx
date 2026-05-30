bdf7ead016fbb5453dda64e7882333726a309e86
bgyordan <yordan.yordanov@csop-varna.bg>
2026-05-30 20:37:03 +0300

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `https://csop-pedagogy.vercel.app/auth/callback`,
         queryParams: {
           hd: 'csop-varna.bg', // restrict to school domain
         },
