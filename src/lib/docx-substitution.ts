b0c391bbb7b8fdde724a969b92efe02d86a7a647
bgyordan <yordan.yordanov@csop-varna.bg>
2026-08-30 22:35:07 +0300

    Change 'Главен счетоводител' to 'Счетоводител'

--- src/lib/docx-substitution.ts
+++ src/lib/docx-substitution.ts
@@ -222,7 +222,7 @@ export async function generateSubstitutionInternalDecl(d: SubstInternalDeclData)
   children.push(new Paragraph({ children: [normal('Посочените часове са действително проведени и са вписани в дневника на класа.', 20)], spacing: { after: 60 } }))
   children.push(new Paragraph({ children: [normal('Дата: …………… 2026 г.            Проверил: ........................  ЗДУД', 22)], spacing: { after: 160 } }))
   children.push(new Paragraph({ children: [normal('Сумата е проверена, начислена и изплатена по ведомост за месец …………… 2026 г.', 20)], spacing: { after: 60 } }))
-  children.push(new Paragraph({ children: [normal('…………………… 2026 година            Главен счетоводител: ........................', 22)] }))
+  children.push(new Paragraph({ children: [normal('…………………… 2026 година            Счетоводител: ........................', 22)] }))
 
   const doc = new Document({ sections: [{ properties: { page: { margin: { top: 720, bottom: 720, left: 800, right: 800 } } }, children }] })
   const blob = await Packer.toBlob(doc)
